a179089fb0cfe0d66c1c081a19053d7906cbc3c0
Ludovico Longhini <ludovico.longhini@gmail.com>
2023-01-24 17:27:53 +0100

    Qualche piccolo problema

---
+++ scripts.js
@@ -0,0 +1,77 @@
+const { createApp } = Vue;
+
+createApp({
+
+    data() {
+
+        return {
+                
+            newTodo: '',
+
+            todos: [ {
+                text: 'Fare la spesa',
+                done: 'False',
+                
+            }, {
+                text: 'Andare in banca',
+                done: 'True',
+                
+            }, {
+                text: 'Chiamare meccanico',
+                done: 'False',
+                
+            }, {
+                text: 'Fare la lavatrice',
+                done: 'True',
+                
+            }, {
+                text: 'Comprare inchiostro nero',
+                done: 'False',
+                
+            }, 
+            ]      
+                
+        }
+    },
+
+    methods: {
+
+        addTodo() {
+
+            if (this.newTodo != 0) {
+
+                this.todos.push(this.newTodo);
+                this.newTodo = '';
+
+            }
+    
+            console.log(this.todos)
+
+        },
+
+        
+        removeTodo: function (index) {
+
+            this.todos.splice(index, 1);
+
+        },
+
+        // check() {
+
+        //     let listItem = document.getElementById('listItem');
+
+        //     if (this.todos.done = 'False') {
+
+        //         listItem.classList.add('check')
+
+        //     } else {
+
+        //         listItem.classList.add('check')
+
+        //     }
+
+        // }
+
+    }
+
+}).mount('#app');
